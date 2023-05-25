f4e58cbeeeeb3f7b224f2957221b1aeb9fef2e89
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-05-24 18:26:56 -0700

    change whole function that split line to array to get all project requirements

--- code_from_line.c
+++ code_from_line.c
@@ -20,45 +20,50 @@ void remove_char(char *str, char c)
 }
 
 /**
- * code_from_line - convert line to arr contain
- * two first code or null
- * @line: line pointer
- * Return: arr of first two code char
+ * code_from_line - function that split line to array of first
+ * two string or null if it is null or space only
+ * @line: pointer to line
+ * Return: pointer to array of strings
  */
 
 char **code_from_line(char *line)
 {
+	char **arr, *token, *ptr;
 	int x = 0;
-	char **arr, *token;
 
-	arr = malloc(sizeof(char *) * 2);
+	if (line == NULL || strlen(line) == 0)
+		return (NULL);
+
+	remove_char(line, '\n');
+	remove_char(line, '\t');
+
+	ptr = malloc(sizeof(char) * (strlen(line) + 1));
+	strcpy(ptr, line);
+	remove_char(ptr, ' ');
+	if (strlen(ptr) == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	free(ptr);
+
+	arr = malloc(sizeof(char *) * 3);
 	if (arr == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	arr[0] = NULL;
-	arr[1] = NULL;
+
 	token = strtok(line, " ");
 	while (token != NULL)
 	{
 		if (x == 2)
 			break;
-
-		arr[x] = malloc(sizeof(token));
-		if (arr[x] == NULL)
-		{
-			free_array(arr);
-			fprintf(stderr, "Error: malloc failed\n");
-			exit(EXIT_FAILURE);
-		}
-		remove_char(token, '$');
-		remove_char(token, '\n');
-		strcpy(arr[x], token);
+		arr[x] = token;
 		x++;
 		token = strtok(NULL, " ");
 	}
+	arr[x] = "\0";
 
 	return (arr);
 }
-
