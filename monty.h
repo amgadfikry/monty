799fda05afa87f7b94bfebc58cb0b4dde1b5045e
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-05-25 08:35:49 -0700

    add prototype of pchar function

--- monty.h
+++ monty.h
@@ -52,5 +52,6 @@ void sub(stack_t **stack, unsigned int line_num);
 void mod(stack_t **stack, unsigned int line_num);
 void mul(stack_t **stack, unsigned int line_num);
 void _div(stack_t **stack, unsigned int line_num);
+void pchar(stack_t **stack, unsigned int line_num);
 
 #endif
