e6149784cdce7073d188c283319377fe72b98d4a
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-05-25 05:21:41 -0700

    add prototypes of stack_len function and swap function

--- monty.h
+++ monty.h
@@ -44,5 +44,7 @@ void pall(stack_t **stack, unsigned int line_num);
 void pint(stack_t **stack, unsigned int line_num);
 void pop(stack_t **stack, unsigned int line_num);
 void free_stack(stack_t *stack);
+int stack_len(stack_t *stack);
+void swap(stack_t **stack, unsigned int line_num);
 
 #endif
